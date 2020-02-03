65a916b44f2cecbd33f63f7401399ed01a534efd
Avin <s10178174@connect.np.edu.sg>
2020-02-03 14:28:59 +0800

    xsd

--- SE assignment/Program.cs
+++ SE assignment/Program.cs
@@ -6,7 +6,7 @@ namespace SE_assignment
     {
         static void Main(string[] args)
         {
-            Customer c = new Customer(1, "Jacob", "Woodlands St 41", "gjia@gmail.com", 94232602);
+            Customer c = new Customer(1, "avin", "Woodlands St 41", "gjia@gmail.com", 94232602);
             c.print();
             Console.ReadLine();
         }
