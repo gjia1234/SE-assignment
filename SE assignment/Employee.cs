73720aa585783f0821ff8846e676b72fdb2dc234
Zhen Wee Darren Yap <s10182207@connect.np.edu.sg>
2020-02-04 16:59:45 +0800

    love

---
+++ SE assignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_assignment
+{
+    class Employee
+    {
+        private string name;
+        private int number;
+        private string nric;
+        private string gender;
+        private int contactNo;
+        private DateTime dateJoin;
+        private string status;
+
+        public Employee(string Name, int Number, string NRIC, string Gender, int ContactNo, DateTime DateJoin, string Status)
+        {
+            name = Name;
+            number = Number;
+            nric = NRIC;
+            gender = Gender;
+            contactNo = ContactNo;
+            dateJoin = DateJoin;
+            status = Status;
+        }
+    }
+}
