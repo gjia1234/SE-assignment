5476f696ea6b0ac77449b421fbe18803f254dcd4
peimian <s10120904@connect.np.edu.sg>
2020-02-04 17:09:10 +0800

    hello

---
+++ SE assignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_assignment
+{
+    class Employee
+{
+    private string Name;
+    private int Number;
+    private string NRIC;
+    private string Gender;
+    private int ContactNo;
+    private DateTime DateJoin;
+    private string Status;
+}
+}
