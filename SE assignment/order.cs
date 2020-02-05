72057344c0156d897e7ceb82fb447b483e12279e
Avin <s10178174@connect.np.edu.sg>
2020-02-05 23:07:04 +0800

    change Order class attributes

--- SE assignment/order.cs
+++ SE assignment/order.cs
@@ -6,23 +6,34 @@ namespace SE_assignment
 {
     class Order
     {
-        private int OrderNo;
-        private string Status; //new, preparing, ready, dispatched, delivered, cancelled
-        private DateTime DateTime; // Date & Time order is submitted
-        private float Subtotal;
-        private float DeliveryCharge;
-        private float GST;
-        private float TotalPaymentAmt;
-        private string PaymentMethod;
-        private DateTime TimeReady; // Time the food is fully prepared
-        private DateTime ETA; // ESTIMATED time the customer will receive the food
-        private DateTime TimeDelivered; // ACTUAL time the customer receive the food
-        private Customer customer;
+        private int orderNo;
+        private string status; //new, preparing, ready, dispatched, delivered, cancelled
+        private DateTime createDateTime; // Date & Time order is submitted
+        private float subtotal;
+        private float deliveryCharge;
+        private float goodServiceTax;
+        private float totalPaymentAmt;
+        private string paymentMethod;
+        private DateTime timeReady; // Time the food is fully prepared
+        private DateTime estimatedTimeArrival; // ESTIMATED time the customer will receive the food
+        private DateTime timeDelivered; // ACTUAL time the customer receive the food
+        private Customer cust;
         
 
-        public Order(int OrderNo, string Status, DateTime DateTime, float Subtotal, float DeliveryCharge, float GST, float TotalPaymentAmt, string PaymentMethod, DateTime TimeReady, DateTime ETA, DateTime TimeDelivered, Customer customer) 
+        public Order(int OrderNo, string Status, DateTime CreateDateTime, float Subtotal, float DeliveryCharge, float GST, float TotalPaymentAmt, string PaymentMethod, DateTime TimeReady, DateTime ETA, DateTime TimeDelivered, Customer customer) 
         {
-            
+            orderNo = OrderNo;
+            status = Status;
+            createDateTime = CreateDateTime;
+            subtotal = Subtotal;
+            deliveryCharge = DeliveryCharge;
+            goodServiceTax = GST;
+            totalPaymentAmt = TotalPaymentAmt;
+            paymentMethod = PaymentMethod;
+            timeReady = TimeReady;
+            estimatedTimeArrival = ETA;
+            timeDelivered = TimeDelivered;
+            cust = customer;
         }
         
 
