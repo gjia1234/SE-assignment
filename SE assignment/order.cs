927a94062ad129e2c71e597ea54e7347ddf8977c
Avin <s10178174@connect.np.edu.sg>
2020-02-05 23:14:31 +0800

    change attribute order class

--- SE assignment/order.cs
+++ SE assignment/order.cs
@@ -11,27 +11,27 @@ namespace SE_assignment
         private DateTime createDateTime; // Date & Time order is submitted
         private float subtotal;
         private float deliveryCharge;
-        private float goodServiceTax;
+        private float GST;
         private float totalPaymentAmt;
         private string paymentMethod;
         private DateTime timeReady; // Time the food is fully prepared
-        private DateTime estimatedTimeArrival; // ESTIMATED time the customer will receive the food
+        private DateTime ETA; // ESTIMATED time the customer will receive the food
         private DateTime timeDelivered; // ACTUAL time the customer receive the food
         private Customer cust;
         
 
-        public Order(int OrderNo, string Status, DateTime CreateDateTime, float Subtotal, float DeliveryCharge, float GST, float TotalPaymentAmt, string PaymentMethod, DateTime TimeReady, DateTime ETA, DateTime TimeDelivered, Customer customer) 
+        public Order(int OrderNo, string Status, DateTime CreateDateTime, float Subtotal, float DeliveryCharge, float goodServiceTax, float TotalPaymentAmt, string PaymentMethod, DateTime TimeReady, DateTime estimatedTimeArrival, DateTime TimeDelivered, Customer customer) 
         {
             orderNo = OrderNo;
             status = Status;
             createDateTime = CreateDateTime;
             subtotal = Subtotal;
             deliveryCharge = DeliveryCharge;
-            goodServiceTax = GST;
+            GST = goodServiceTax;
             totalPaymentAmt = TotalPaymentAmt;
             paymentMethod = PaymentMethod;
             timeReady = TimeReady;
-            estimatedTimeArrival = ETA;
+            ETA = estimatedTimeArrival;
             timeDelivered = TimeDelivered;
             cust = customer;
         }
