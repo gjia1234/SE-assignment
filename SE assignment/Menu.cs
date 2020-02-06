535a5e355f18048d549c4ad9d4dd137c300b36cc
Cjeng Jacob Lee <s10177571@connect.np.edu.sg>
2020-02-06 11:29:14 +0800

    made some changes

---
+++ SE assignment/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_assignment
+{
+    class Menu
+    {
+        private int id;
+        private string name;
+        private string description;
+        private int price;
+        private int unit;
+        private string status;
+    }
+}
