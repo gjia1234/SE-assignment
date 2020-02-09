1169910edfbfbe236f8e9d7a59dc107cc997b794
Cjeng Jacob Lee <s10177571@connect.np.edu.sg>
2020-02-09 11:53:23 +0800

    finished doing customer and iterator pattern

--- SE assignment/Program.cs
+++ SE assignment/Program.cs
@@ -10,13 +10,14 @@ namespace SE_assignment
 
             //Test Data for Order
             //Test Data 1
+
             Customer cust1 = new Customer(1, "Avin", "Ngee Ann Poly", "avin@np.com", 91234567);
             Dispatcher disp1 = new Dispatcher("mohd", 1, "S9912313C", "Male", 97654321, DateTime.Now, "new");
             List<OrderLine> orderLineList1 = new List<OrderLine>();
 
             Menu m1 = new Menu(1, "Chicken Nugget", "Tasty Chicken Nugget", 12, 1, "Available");
             OrderLine ol1 = new OrderLine(1, m1);
-            Menu m2 = new Menu(1, "Chicken Whole", "Tasty Chicken ", 12, 1, "Available");
+            Menu m2 = new Menu(1, "Whole Chicken", "Tasty Chicken ", 12, 1, "Available");
             OrderLine ol2 = new OrderLine(2, m2);
 
             orderLineList1.Add(ol1);
@@ -29,15 +30,20 @@ namespace SE_assignment
             Dispatcher disp2 = new Dispatcher("Ali", 1, "S9912313C", "Male", 97654321, DateTime.Now, "new");
             List<OrderLine> orderLineList2 = new List<OrderLine>();
 
-            Menu m3 = new Menu(1, "Chicken Nugget", "Tasty Chicken Nugget", 12, 1, "Available");
+            Menu m3 = new Menu(1, "Chicken Burger", "Tasty Chicken Nugget", 12, 1, "Available");
             OrderLine ol3 = new OrderLine(1, m1);
-            Menu m4 = new Menu(1, "Chicken Whole", "Tasty Chicken ", 12, 1, "Available");
+            Menu m4 = new Menu(1, "Pasta", "Tasty Chicken ", 12, 1, "Available");
             OrderLine ol4 = new OrderLine(2, m2);
 
             orderLineList2.Add(ol1);
             orderLineList2.Add(ol2);
-
             Order b = new Order(2, "New", DateTime.Now, "Cash", cust2, orderLineList1);
+
+            Globals.MenuList.Add(m1);
+            Globals.MenuList.Add(m2);
+            Globals.MenuList.Add(m3);
+            Globals.MenuList.Add(m4);
+            Globals.CustomerList.Add(cust1);
             Globals.OrderList.Add(a);
             Globals.OrderList.Add(b);
 
