3d79b96ccce2de7a7795cea7f14f1e711e286543
Avin <s10178174@connect.np.edu.sg>
2020-02-09 20:23:35 +0800

    remove test data

--- SE assignment/Program.cs
+++ SE assignment/Program.cs
@@ -7,42 +7,7 @@ namespace SE_assignment
     {
         static void Main(string[] args)
         {   
-
-            //Test Data for Order
-            //Test Data 1
-            Customer cust1 = new Customer(1, "Avin", "Ngee Ann Poly", "avin@np.com", 91234567);
-            Dispatcher disp1 = new Dispatcher("mohd", 1, "S9912313C", "Male", 97654321, DateTime.Now, "new");
-            List<OrderLine> orderLineList1 = new List<OrderLine>();
-
-            Menu m1 = new Menu(1, "Chicken Nugget", "Tasty Chicken Nugget", 12, 1, "Available");
-            OrderLine ol1 = new OrderLine(1, m1);
-            Menu m2 = new Menu(1, "Chicken Whole", "Tasty Chicken ", 12, 1, "Available");
-            OrderLine ol2 = new OrderLine(2, m2);
-
-            orderLineList1.Add(ol1);
-            orderLineList1.Add(ol2);
-
-            Order a = new Order(1, "New", DateTime.Now, "Credit Card", cust1, orderLineList1);
-
-            //Test Data 2
-            Customer cust2 = new Customer(1, "Diego", "Ngee Ann Poly", "avin@np.com", 91234567);
-            Dispatcher disp2 = new Dispatcher("Ali", 1, "S9912313C", "Male", 97654321, DateTime.Now, "new");
-            List<OrderLine> orderLineList2 = new List<OrderLine>();
-
-            Menu m3 = new Menu(1, "Chicken Nugget", "Tasty Chicken Nugget", 12, 1, "Available");
-            OrderLine ol3 = new OrderLine(1, m1);
-            Menu m4 = new Menu(1, "Chicken Whole", "Tasty Chicken ", 12, 1, "Available");
-            OrderLine ol4 = new OrderLine(2, m2);
-
-            orderLineList2.Add(ol1);
-            orderLineList2.Add(ol2);
-
-            Order b = new Order(2, "New", DateTime.Now, "Cash", cust2, orderLineList1);
-            Globals.OrderList.Add(a);
-            Globals.OrderList.Add(b);
-
-
-            //Message
+                        //Message
             Console.Write("What is your role? \n[1]Customer\n[2]Manager\n[3]Chef\nInput Option: ");
             string role = Console.ReadLine();
             if (role == "1")
